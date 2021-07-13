f43c45470bec32ab8c3474f6a30bde5a9da487d5
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-07-13 17:49:50 +0530

    test and code checkbox functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 
 function App() {
   const [bgColor, setBgColor] = useState('red');
+  const [isChecked, setIsChecked] = useState(false);
 
   const background = bgColor === 'red' ? 'blue' : 'red';
 
   return (
     <div>
+      <input 
+        type="checkbox" 
+        onClick={(e)=>{setIsChecked(e.target.checked)}}
+      />
       <button
         style={{ backgroundColor: bgColor }}
+        disabled={isChecked}
         onClick={()=>{
           setBgColor(background)
         }}
--- src/App.test.js
+++ src/App.test.js
@@ -12,9 +12,26 @@ test('button have correct intial color', () => {
   fireEvent.click(buttonColor);
 
   // expect to have a background color of blue
-  expect(buttonColor).toHaveStyle({backgroundColor: 'blue'});
+  expect(buttonColor).toHaveStyle({ backgroundColor: 'blue' });
 
   // assertion for the text of button
   expect(buttonColor.textContent).toBe('Change to red')
 
 });
+
+test("intitial condition =>", () => {
+
+  render(<App />);
+  const ButtonColor = screen.getByRole('button', { name: 'Change to blue' });
+
+  expect(ButtonColor).toBeEnabled();
+
+  const checkBox = screen.getByRole('checkbox');
+  expect(checkBox).not.toBeChecked();
+
+  fireEvent.click(checkBox);
+
+  expect(ButtonColor).toBeDisabled();
+})
+
+
